62ace931b6b621f729c0e6e9670db1b036429445
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-09-05 18:11:57 +0100

    Added datatables to index page table and retrieve the data using the API controller

---
+++ wwwroot/js/index.js
@@ -0,0 +1,53 @@
+﻿/// <reference types="datatables.net" />
+
+var table = $("table#master-table").DataTable({
+	data: [],
+	columns: [
+		{
+			name: "_id",
+			data: "id",
+			visible: false
+		},
+		{
+			title: "First Name",
+			data: "firstName"
+		},
+		{
+			title: "Last Name",
+			data: "lastName"
+		},
+		{
+			title: "Organisation",
+			data(row) {
+				return row.organisation.organisationName;
+			}
+		},
+		{
+			title: "Organisation Number",
+			data: "organisationNumber"
+		}
+	]
+});
+
+loadData();
+
+async function loadData() {
+	const organisations = await rest.get("/api/Organisation");
+	const employees = await rest.get("/api/Employee");
+
+	const orgMap = new Map();
+	for (const org of organisations)
+		orgMap.set(org.organisationNumber, org);
+
+	const data = employees.map(({ employeeId: id, organisationNumber, firstName, lastName }) => {
+		return {
+			id,
+			firstName,
+			lastName,
+			organisationNumber,
+			organisation: orgMap.get(organisationNumber)
+		}
+	});
+
+	table.rows.add(data).draw();
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,83 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+class HttpError extends Error {
+	get status() {
+		return this.body.status;
+	}
+
+	get statusText() {
+		return this.body.statusText;
+	}
+
+	/**
+	 * @param {Response} body
+	 */
+	constructor(body) {
+		super("Http request failed with status code " + body.status);
+		this.body = body;
+	}
+}
+
+/**
+ * @param {"GET" | "POST" | "PUT" | "DELETE"} method
+ * @param {string} url
+ * @param {any} [body]
+ * @returns
+ */
+async function rest(method, url, body) {
+	const headers = {};
+	if (body !== undefined) {
+		body = JSON.stringify(body);
+		headers["content-type"] = "application/json";
+	}
+
+	const response = await fetch(url, {
+		method,
+		body
+	});
+
+	if (!response.ok)
+		throw new HttpError(response);
+
+	return await response.json();
+}
+
+/**
+ * @template {any} T
+ * @param {string} url
+ * @returns {Promise<T>}
+ */
+rest.get = function (url) {
+	return rest("GET", url);
+}
+
+/**
+ * @template {any} T
+ * @param {string} url
+ * @returns {Promise<T>}
+ */
+rest.delete = function (url) {
+	return rest("DELETE", url);
+}
+
+/**
+ * @template {any} T
+ * @param {string} url
+ * @param {any} body
+ * @returns {Promise<T>}
+ */
+rest.post = function (url, body) {
+	return rest("POST", url, body);
+}
+
+/**
+ * @template {any} T
+ * @param {string} url
+ * @param {any} body
+ * @returns {Promise<T>}
+ */
+rest.put = function (url, body) {
+	return rest("PUT", url, body);
+}
