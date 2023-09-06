75932df5a6f96702629231816d8cb4b92da5b629
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-09-06 16:10:16 +0100

    Removed unused code from page models

--- Pages/Employees.cshtml.cs
+++ Pages/Employees.cshtml.cs
@@ -1,27 +1,11 @@
-﻿using Demo.Website.Data;
-using Demo.Website.Data.Entities;
-
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Demo.Website.Pages
 {
 	public class EmployeesModel : PageModel
 	{
-		private readonly ILogger<EmployeesModel> _logger;
-		private readonly AppDbContext _context;
-
-		public IReadOnlyList<Employee> Employees { get; private set; } = Array.Empty<Employee>();
-
-		public EmployeesModel(ILogger<EmployeesModel> logger, AppDbContext context)
+		public EmployeesModel()
 		{
-			_logger = logger;
-			_context = context;
-		}
-
-		public async Task OnGetAsync()
-		{
-			Employees = await _context.Employees.Include(v => v.Organisation).ToArrayAsync();
 		}
 	}
 }
--- Pages/Organisations.cshtml.cs
+++ Pages/Organisations.cshtml.cs
@@ -1,25 +1,11 @@
-﻿using Demo.Website.Data;
-using Demo.Website.Data.Entities;
-
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Demo.Website.Pages
 {
 	public class OrganisationsModel : PageModel
 	{
-		private readonly AppDbContext _context;
-
-		public IReadOnlyList<Organisation> Organisations { get; private set; } = Array.Empty<Organisation>();
-
-		public OrganisationsModel(AppDbContext context)
+		public OrganisationsModel()
 		{
-			_context = context;
-		}
-
-		public async Task OnGetAsync()
-		{
-			Organisations = await _context.Organisations.ToListAsync();
 		}
 	}
 }
